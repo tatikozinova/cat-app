58fa59df7ef297c70444f79f674961fd4a2e9813
Tetians Kozinova <tetianskozinova@Tetianss-MacBook-Air.local>
2023-01-29 15:58:39 -0800

    your commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const[cat, setCat] = useState(0);
+ const{catBreed, image, description}=data[cat];
+ 
+
+ const[showMore, setShowMore]=useState(false);
+  const[showText, setShowText]=useState(false);
+
+ const showTextClick=(element)=>{
+  element.showMore=!element.showMore
+  setShowText(!showText)
+ }
+
+ const previousCat=()=>{
+  setCat((cat=>{
+    cat --;
+    if(cat<0) {
+      return data.length -1;
+    }
+    return cat;
+  }))
+ }
+
+const nextCat=()=>{
+  setCat((cat=>{
+    cat ++;
+    if(cat>data.length -1){
+      cat=0
+    }
+    return cat;
+  }))
+}
+
+return (<div>
+
+
+  <div className='container'>
+    <img src={image} width="300px" alt="kitty"/>
+  </div>
+
+  
+
+  <div className='container'>
+    <h2>{catBreed}</h2>
+  </div>
+
+  <div className='container'>
+    <h2>{description}</h2>
+  </div>
+
+  <div className='container'>
+      <p>{showMore ? description : description.substring(0,40)+'...'}<button onClick={()=>showTextClick(element)}>{showMore ? 'Show less' : 'Show more'}</button></p>
     </div>
-  );
+
+
+  <div className='btn container'>
+    <button onClick={previousCat}>Previous</button>
+    <button onClick={nextCat}>Next</button>
+  </div>
+</div>)
+
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const data=[
+    {
+        catBreed: "Highland Lynx",
+        image:"http://www.americanexoticcats.com/uploads/1/5/3/5/15352318/3102788.jpg?298",
+        description:"The Highlander is a new breed of cat. The unique appearance of the Highlander comes from the deliberate cross between the Desert Lynx and the Jungle Curl breeds, also recently developed."
+    },
+    {
+        catBreed:"Bengal",
+        image:"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F04%2F20%2Fbengal-wood-walking-867775498-2000.jpg",
+        description:"The Bengal cat is a domesticated cat breed created from a hybrid of the Asian leopard cat, with domestic cats, especially the spotted Egyptian Mau. The breed name comes from the leopard cat's taxonomic name."
+    },
+    {
+        catBreed:"Siberian",
+        image:"https://www.purina.com/sites/default/files/styles/kraken_generic_max_width_480/public/Siberian_body_6.jpg?itok=fytGUI_W",
+        description:"The Siberian is a centuries-old landrace of domestic cat in Russia and recently developed as a formal breed with standards promulgated the world over since the late 1980s. Siberians vary from medium to medium-large in size."
+    },
+
+]
